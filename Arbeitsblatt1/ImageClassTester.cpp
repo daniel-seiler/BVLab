98ceaecab9523e9314bc6a69098afca891f3a9ef
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2022-11-09 15:39:53 +0100

    added files for task 1

---
+++ Arbeitsblatt1/ImageClassTester.cpp
@@ -0,0 +1,13 @@
+// ImageClassTester.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "MyImage.h"
+#include "MyHisto.h"
+
+int main(int argc, char *argv[])
+{
+  CMyImage testImage;
+
+  return 0;
+}
+
