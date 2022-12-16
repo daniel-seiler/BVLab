0058b1548b93b43c605cb71e3c6e50334825de34
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2022-12-16 12:26:51 +0100

    added indentation

--- BVProject/main.cpp
+++ BVProject/main.cpp
@@ -6,25 +6,25 @@
 #include <opencv2/imgproc.hpp>
 
 void captureFrame(cv::VideoCapture* cap, cv::Mat *frame) {
-  cap->read(*frame);
-  if (frame->empty()) {
-    std::cerr << "ERROR! blank frame grabbed" << std::endl;
-  }
+    cap->read(*frame);
+    if (frame->empty()) {
+        std::cerr << "ERROR! blank frame grabbed" << std::endl;
+    }
 }
 
 void matchCircles(std::vector<cv::Vec3f> *circles, cv::Mat *frame) {
-  cv::Mat preparedFrame;
-  std::vector<cv::Vec3f> tmpCircles;
+    cv::Mat preparedFrame;
+    std::vector<cv::Vec3f> tmpCircles;
 
-  //prepare frame
-  cv::cvtColor(*frame, preparedFrame, 6);
-  cv::medianBlur(preparedFrame, preparedFrame, 5);
+    //prepare frame
+    cv::cvtColor(*frame, preparedFrame, 6);
+    cv::medianBlur(preparedFrame, preparedFrame, 5);
 
-  //fit circles
-  HoughCircles(preparedFrame, tmpCircles, cv::HOUGH_GRADIENT, 1, 15, 25, 90, 40, 200);
+    //fit circles
+    HoughCircles(preparedFrame, tmpCircles, cv::HOUGH_GRADIENT, 1, 15, 25, 90, 40, 200);
 
-  //add to existing circles
-  circles->insert(circles->end(), tmpCircles.begin(), tmpCircles.end());
+    //add to existing circles
+    circles->insert(circles->end(), tmpCircles.begin(), tmpCircles.end());
 
 }
 
@@ -40,11 +40,11 @@ void visualize(std::vector<cv::Vec3f> *circles, cv::Mat *frame) {
     //std::cout << radius << std::endl;
   }
 
-  cv::imshow("Live", *frame);
+    cv::imshow("Live", *frame);
 }
 
 bool compareCircles(const cv::Vec3f &a, const cv::Vec3f &b) {
-  return a[2] < b[2];
+    return a[2] < b[2];
 }
 
 void maxDiff(std::vector<cv::Vec3f> *circles, std::vector<cv::Vec3f> *set1, std::vector<cv::Vec3f> *set2) {
@@ -53,47 +53,57 @@ void maxDiff(std::vector<cv::Vec3f> *circles, std::vector<cv::Vec3f> *set1, std:
 
 int main(int, char**)
 {
-  //--- INITIALIZE VIDEOCAPTURE
-  cv::VideoCapture cap;
-  // open the default camera using default API
-  // cap.open(0);
-  // OR advance usage: select any API backend
-  int deviceID = 4;             // 0 = open default camera
-  int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-  // open selected camera using selected API
-  cap.open(deviceID, apiID);
-  // check if we succeeded
-  if (!cap.isOpened()) {
-    std::cerr << "ERROR! Unable to open camera\n";
-    return -1;
-  }
-  //--- GRAB AND WRITE LOOP
-  std::cout << "Start grabbing" << std::endl
-       << "Press any key to terminate" << std::endl;
-  while(42) {
-    std::vector<cv::Vec3f> circles;
-    cv::Mat frame;
-
-    //TODO replace for with while(numberOfCircles)
-    for(int i = 0; i < 5; i++) {
-      captureFrame(&cap, &frame);
-      matchCircles(&circles, &frame);
-
-      //sort vector
-      std::sort(circles.begin(), circles.end(), compareCircles);
-
-      if (cv::waitKey(5) >= 0)
-        return 0;
+    //--- INITIALIZE VIDEOCAPTURE
+    cv::VideoCapture cap;
+    // open the default camera using default API
+    // cap.open(0);
+    // OR advance usage: select any API backend
+    int deviceID = 0;             // 0 = open default camera
+    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    // open selected camera using selected API
+    cap.open(deviceID, apiID);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        std::cerr << "ERROR! Unable to open camera\n";
+        return -1;
     }
+    //--- GRAB AND WRITE LOOP
+    std::cout << "Start grabbing" << std::endl
+              << "Press any key to terminate" << std::endl;
+    while(42) {
+        std::vector<cv::Vec3f> circles;
+        cv::Mat frame;
 
-    std::cout << std::endl << std::endl;
-    for(int i = 0; i < circles.size(); i++) {
-      std::cout << circles[i][2] << std::endl;
+        //TODO replace for with while(numberOfCircles)
+        for(int i = 0; i < 5; i++) {
+            captureFrame(&cap, &frame);
+            matchCircles(&circles, &frame);
+
+            //sort vector
+            std::sort(circles.begin(), circles.end(), compareCircles);
+
+            if (cv::waitKey(5) >= 0)
+                return 0;
+        }
+
+        std::vector<cv::Vec3f> set1(circles.size());
+        std::vector<cv::Vec3f> set2(circles.size());
+        int splitIndex = maxDiff(&circles);
+        cv::Vec3f c1, c2;
+        //calcMeanCircle(&set1, &c1);
+        //calcMeanCircle(&set2, &c2);
+
+        //visualize(&c1, &frame);
+        //visualize(&c2, &frame);
+
+        std::cout << std::endl << std::endl;
+        for(int i = 0; i < circles.size(); i++) {
+            std::cout << circles[i][2] << std::endl;
+        }
+        std::cout << "Split index: " << splitIndex << std::endl << std::endl;
+
+        visualizeVec(&circles, &frame);
     }
-    std::cout << std::endl << std::endl;
-
-    visualize(&circles, &frame);
-  }
-  // the camera will be deinitialized automatically in VideoCapture destructor
-  return 0;
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
 }
