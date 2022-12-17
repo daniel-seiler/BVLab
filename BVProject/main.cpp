ced6543f90c328da13b6a1905e6ae5cb99b9ab9f
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2022-12-17 01:32:39 +0100

    finished up the dividing of a set of circles into two subsets

--- BVProject/main.cpp
+++ BVProject/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <opencv2/imgproc.hpp>
+#include <deque>
 
 void captureFrame(cv::VideoCapture* cap, cv::Mat *frame) {
     cap->read(*frame);
@@ -12,7 +13,7 @@ void captureFrame(cv::VideoCapture* cap, cv::Mat *frame) {
     }
 }
 
-void matchCircles(std::vector<cv::Vec3f> *circles, cv::Mat *frame) {
+void matchCircles(std::deque<cv::Vec3f> *circles, cv::Mat *frame) {
     cv::Mat preparedFrame;
     std::vector<cv::Vec3f> tmpCircles;
 
@@ -21,32 +22,34 @@ void matchCircles(std::vector<cv::Vec3f> *circles, cv::Mat *frame) {
     cv::medianBlur(preparedFrame, preparedFrame, 5);
 
     //fit circles
-    HoughCircles(preparedFrame, tmpCircles, cv::HOUGH_GRADIENT, 1, 15, 25, 90, 40, 200);
+    HoughCircles(preparedFrame, tmpCircles, cv::HOUGH_GRADIENT, 1, 1, 25, 90, 40, 200);
 
     //add to existing circles
     circles->insert(circles->end(), tmpCircles.begin(), tmpCircles.end());
 
 }
 
-void visualize(cv::Vec3f *c, cv::Mat *frame) {
+void visualize(cv::Vec3f *c, cv::Mat *frame, cv::Scalar col) {
     cv::Point center = cv::Point((*c)[0], (*c)[1]);
 
     circle(*frame, center, 1, cv::Scalar(0, 100, 100), 3, cv::LINE_AA);
 
     int radius = (*c)[2];
-    circle(*frame, center, radius, cv::Scalar(255, 0, 255), 3, cv::LINE_AA);
+    circle(*frame, center, radius, col, 3, cv::LINE_AA);
 }
 
-void visualizeVec(std::vector<cv::Vec3f> *circles, cv::Mat *frame) {
+void visualize(cv::Vec3f *c, cv::Mat *frame) {
+    visualize(c, frame, cv::Scalar(255, 0, 255));
+}
+
+void visualizeVec(std::deque<cv::Vec3f> *circles, cv::Mat *frame) {
     for (size_t i = 0; i < circles->size(); i++) {
         cv::Vec3i c = (*circles)[i];
-        visualize(&(*circles)[i], frame);
+        visualize(&(*circles)[i], frame, cv::Scalar(0, 100, 100));
     }
-
-    cv::imshow("Live", *frame);
 }
 
-void calcMeanCircle(std::vector<cv::Vec3f> *set, cv::Vec3f *circle) {
+void calcMeanCircle(std::deque<cv::Vec3f> *set, cv::Vec3f *circle) {
     double radius, x, y = 0.0;
     for (auto &tmpCircle: *set) {
         x += tmpCircle[0];
@@ -57,35 +60,42 @@ void calcMeanCircle(std::vector<cv::Vec3f> *set, cv::Vec3f *circle) {
     y /= set->size();
     radius /= set->size();
 
-    circle[0] = x;
-    circle[1] = y;
-    circle[2] = radius;
-    std::cout << radius << std::endl;
+    (*circle)[0] = x;
+    (*circle)[1] = y;
+    (*circle)[2] = radius;
 }
 
-bool compareCircles(const cv::Vec3f &a, const cv::Vec3f &b) {
-    return a[2] < b[2];
-}
+int maxDiff(std::deque<cv::Vec3f> *circles, cv::Vec3f *avgCircle1, cv::Vec3f *avgCircle2) {
+    //sort vector
+    std::sort(circles->begin(), circles->end(), [](const cv::Vec3f &a, const cv::Vec3f &b) {
+        return a[2] > b[2];
+    });
 
-int maxDiff(std::vector<cv::Vec3f> *circles) {
-    int bestIndex = -1;
-    double maxDiff = 0.0;
-    for (int i = 0; i < circles->size(); i++) {
-        std::vector<cv::Vec3f> set1(circles->begin(), circles->begin() + i);
-        std::vector<cv::Vec3f> set2(circles->begin() + i, circles->end());
-        cv::Vec3f tmpC1;
-        cv::Vec3f tmpC2;
-        calcMeanCircle(&set1, &tmpC1);
-        calcMeanCircle(&set2, &tmpC2);
-        if (tmpC1[2] > 0 && tmpC1[2] < 300 && tmpC2[2] > 0 && tmpC2[2] < 300) {
-            double tmpDiff = tmpC1[2] - tmpC2[2];
-            if (tmpDiff > maxDiff) {
-                maxDiff = tmpDiff;
-                bestIndex = i;
-            }
+    std::deque<cv::Vec3f> set1, set2;
+    double sum1, sum2;
+    cv::Vec3f front = circles->front();
+    circles->pop_front();
+    set1.push_front(front);
+    sum1 = front[2];
+    cv::Vec3f back = circles->back();
+    circles->pop_back();
+    set2.push_front(front);
+    sum2 = back[2];
+
+    while (!circles->empty()) {
+        cv::Vec3f val = circles->front();
+        circles->pop_front();
+        if (abs(front[2] - val[2]) < abs(back[2] - val[2])) {
+            set1.push_front(val);
+            sum1 += val[2];
+        } else {
+            set2.push_front(val);
+            sum2 += val[2];
         }
     }
-    return bestIndex;
+
+    calcMeanCircle(&set1, avgCircle1);
+    calcMeanCircle(&set2, avgCircle2);
 }
 
 int main(int, char**)
@@ -108,38 +118,30 @@ int main(int, char**)
     std::cout << "Start grabbing" << std::endl
               << "Press any key to terminate" << std::endl;
     while(42) {
-        std::vector<cv::Vec3f> circles;
+        std::deque<cv::Vec3f> circles;
         cv::Mat frame;
 
-        //TODO replace for with while(numberOfCircles)
-        for(int i = 0; i < 5; i++) {
+        while(circles.size() < 5) {
             captureFrame(&cap, &frame);
+            resize(frame, frame, cv::Size(), 0.15, 0.15);
             matchCircles(&circles, &frame);
 
-            //sort vector
-            std::sort(circles.begin(), circles.end(), compareCircles);
-
-            if (cv::waitKey(5) >= 0)
-                return 0;
+            //if (cv::waitKey(5) >= 0)
+            //    return 0;
+            cv::waitKey(50);
         }
 
-        std::vector<cv::Vec3f> set1(circles.size());
-        std::vector<cv::Vec3f> set2(circles.size());
-        int splitIndex = maxDiff(&circles);
-        cv::Vec3f c1, c2;
-        //calcMeanCircle(&set1, &c1);
-        //calcMeanCircle(&set2, &c2);
+        cv::Vec3f avgCircle1, avgCircle2;
+        //visualizeVec(&circles, &frame);
+        maxDiff(&circles, &avgCircle1, &avgCircle2);
 
-        //visualize(&c1, &frame);
-        //visualize(&c2, &frame);
+        visualize(&avgCircle1, &frame);
+        visualize(&avgCircle2, &frame);
 
-        std::cout << std::endl << std::endl;
-        for(int i = 0; i < circles.size(); i++) {
-            std::cout << circles[i][2] << std::endl;
-        }
-        std::cout << "Split index: " << splitIndex << std::endl << std::endl;
+        cv::imshow("Live", frame);
 
-        visualizeVec(&circles, &frame);
+        if (cv::waitKey(50) >= 0)
+            break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
