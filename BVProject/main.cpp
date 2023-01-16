cc3ea72efc09b8d5a5bbce9b27509ac5397733cd
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2023-01-16 18:43:50 +0100

    added newest changes

--- BVProject/main.cpp
+++ BVProject/main.cpp
@@ -6,11 +6,27 @@
 #include <opencv2/imgproc.hpp>
 #include <deque>
 
+#define LINE_THICKNESS 1
+#define R_EMPTY 70      //in px
+#define R_FULL 80.8     //in px
+#define R_CUP_TOP 97.4  //in px
+#define HIGHT_CAM 270   //in mm
+#define HIGHT_CUP 90    //in mm
+#define R_TOP 50        //in mm
+
 void captureFrame(cv::VideoCapture* cap, cv::Mat *frame) {
     cap->read(*frame);
     if (frame->empty()) {
         std::cerr << "ERROR! blank frame grabbed" << std::endl;
     }
+}
+
+float avgVector(std::vector<float> *v) {
+    float avg = 0;
+    for (auto &tmp : *v) {
+        avg += tmp;
+    }
+    return avg / v->size();
 }
 
 void matchCircles(std::vector<cv::Vec3f> *circles, cv::Mat *frame) {
@@ -22,7 +38,7 @@ void matchCircles(std::vector<cv::Vec3f> *circles, cv::Mat *frame) {
     cv::medianBlur(preparedFrame, preparedFrame, 5);
     cv::Mat edges;
     Canny(preparedFrame, edges, 100, 200, 5, false);
-    cv::imshow("First", edges);
+    //cv::imshow("First", edges);
     //fit circles
     HoughCircles(edges, tmpCircles, cv::HOUGH_GRADIENT, 1, 1, 25, 125, 40, 200);
 
@@ -34,10 +50,10 @@ void matchCircles(std::vector<cv::Vec3f> *circles, cv::Mat *frame) {
 void visualize(cv::Vec3f *c, cv::Mat *frame, cv::Scalar col) {
     cv::Point center = cv::Point((*c)[0], (*c)[1]);
 
-    circle(*frame, center, 1, cv::Scalar(0, 100, 100), 3, cv::LINE_AA);
+    circle(*frame, center, 1, cv::Scalar(0, 100, 100), LINE_THICKNESS, cv::LINE_AA);
 
     int radius = (*c)[2];
-    circle(*frame, center, radius, col, 3, cv::LINE_AA);
+    circle(*frame, center, radius, col, LINE_THICKNESS, cv::LINE_AA);
 }
 
 void visualize(cv::Vec3f *c, cv::Mat *frame) {
@@ -58,6 +74,17 @@ int cluster(std::vector<cv::Vec3f> *circles, cv::Vec3f *avgCircle1, cv::Vec3f *a
 
     *avgCircle1 = centers.at(0);
     *avgCircle2 = centers.at(1);
+}
+
+void calibrate(cv::Vec3f *c1, cv::Vec3f *c2, std::vector<float> *m) {
+    //m->push_back(abs((*c2)[2] - (*c1)[2]));
+    m->push_back(std::min((*c2)[2], (*c1)[2]));
+}
+
+float calcVolume(cv::Vec3f *c1, cv::Vec3f *c2) {
+    float r_max = std::max((*c1)[2], (*c2)[2]);
+    float r_coffee = std::min((*c1)[2], (*c2)[2]);
+    return ((r_max - R_EMPTY) - (r_max - r_coffee))  / (r_max - R_FULL);
 }
 
 int main(int, char**)
@@ -86,18 +113,20 @@ int main(int, char**)
     //--- GRAB AND WRITE LOOP
     std::cout << "Start grabbing" << std::endl
               << "Press any key to terminate" << std::endl;
+
+    std::vector<float> measurements;
     while(42) {
         std::vector<cv::Vec3f> circles;
         cv::Mat frame;
 
-        while(circles.size() < 5) {
+        while(circles.size() < 50) {
             captureFrame(&cap, &frame);
             //resize(frame, frame, cv::Size(), 0.15, 0.15);
             matchCircles(&circles, &frame);
 
             //if (cv::waitKey(5) >= 0)
             //    return 0;
-            cv::waitKey(50);
+            cv::waitKey(20);
             //visualizeVec(&circles, &frame);
             //cv::imshow("First", frame);
         }
@@ -105,15 +134,21 @@ int main(int, char**)
         cv::Vec3f avgCircle1, avgCircle2;
         //visualize(&circles, &frame);
         cluster(&circles, &avgCircle1, &avgCircle2);
+        //maxDiff(&circles, &avgCircle1, &avgCircle2);
 
         visualize(&avgCircle1, &frame);
         visualize(&avgCircle2, &frame);
+
+        calibrate(&avgCircle1, &avgCircle2, &measurements);
+        std::cout << "Volume percent: " << calcVolume(&avgCircle1, &avgCircle2) * 100 << "%" << std::endl;
 
         cv::imshow("Live", frame);
 
         if (cv::waitKey(50) >= 0)
             break;
     }
+
+    std::cout << "Average measurement: " << avgVector(&measurements) << " amount values: " << measurements.size() << std::endl;
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
